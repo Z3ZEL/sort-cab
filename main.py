ee253a79f9a4be77b9cca51133e0a4d5bb03cff6
Z3ZEL <estebanrodriguez17780@gmail.com>
2022-12-23 19:17:24 +0100

    Version 1.1 Pdf opened

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 import tkinter as tk
-
+import PyPDF2
+import subprocess
 import os.path
 import json
 from google.auth.transport.requests import Request
@@ -16,7 +19,48 @@ import datetime
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-def char_to_int(char: str) -> int:
+def retrieve_pdf_info(file_path):
+    '''Retrieve the pdf info from the file_path.''' 
+    pdfFileObj = open(file_path, 'rb')
+    subprocess.Popen([file_path],shell=True)
+def retrieve_folders(service, root_folder_name):
+    '''Retrieve the folders from the root folder.'''
+    # Retrieve the root folder
+    query = "mimeType='application/vnd.google-apps.folder' and trashed = false and name='" + root_folder_name + "'"
+    results = service.files().list(q=query,fields="nextPageToken, files(id, name)").execute()
+    files = results.get("files", [])
+    if not files:
+        print('No root folder found.')
+        return
+    else:
+        root_folder = files[0]
+        print('Root folder found: ' + root_folder["name"] + ' (' + root_folder["id"] + ')')
+    # Retrieve the folders from the root folder 
+    query = "mimeType='application/vnd.google-apps.folder' and trashed = false and parents in '" + root_folder["id"] + "'"
+    results = service.files().list(q=query,fields="nextPageToken, files(id, name)").execute()
+    files = results.get("files", [])
+    if not files:
+        print('No folders found.')
+        return
+    else:
+        print('Folders found:')
+        for file in files:
+            print(file["name"] + ' (' + file["id"] + ')')
+    return files
+def retrieve_folder_from_name(service, folder_name):
+    '''Retrieve a folder from its name.'''
+    # Retrieve the folder
+    query = "mimeType='application/vnd.google-apps.folder' and trashed = false and name='" + folder_name + "'"
+    results = service.files().list(q=query,fields="nextPageToken, files(id, name)").execute()
+    files = results.get("files", [])
+    if not files:
+        print('No folder found.')
+        return
+    else:
+        folder = files[0]
+        print('Folder found: ' + folder["name"] + ' (' + folder["id"] + ')')
+    return folder
+def char_to_int(char):
     """Convert a single character to an integer.
 
     Args:
@@ -31,6 +75,7 @@ def find_folder(folder, folders):
         if f.id == folder.id:
             return f
     return None
+
 #CLASS
 class Windows:
     '''Single object to manage the windows.
@@ -47,10 +92,11 @@ class Windows:
         write: Write text in the dialog box label.
         clear: Clear the dialog box label.
     '''
-    def __init__(self, window,send_button, dialog_box):
+    def __init__(self, window,send_button, dialog_box, pdf_info):
         self.send_button = send_button
         self.dialog_box = dialog_box
         self.window = window
+        self.pdf_info = pdf_info
     def redraw(self):
         self.window.update()
     def write(self, text):
@@ -60,6 +106,9 @@ class Windows:
         self.dialog_box.delete(1.0, tk.END)
     def change_button_color(self, color):
         self.send_button.config(bg=color)
+    def write_pdf_info(self, text):
+        self.pdf_info.configure(text= text)
+
 class FileOrganiser:
     '''Single object to manage the file upload to the Google Drive right folder.
     
@@ -77,8 +126,7 @@ class FileOrganiser:
         send_file_to: send the file to the right folder.
     '''
 
-    def __init__(self, folders, service, windows=None):
-        self.folders = folders
+    def __init__(self, service, windows=None):
         self.file_to_sort = None
         self.service = service
         self.windows = windows
@@ -88,7 +136,7 @@ class FileOrganiser:
         self.file_to_sort = file
     def upload_file(self, file_path, folder_id, file_name, subfolder_name):
         '''
-        Upload a file to a folder.
+        Upload a file to a folder, according to specific root folder
 
         Args:
 
@@ -96,6 +144,7 @@ class FileOrganiser:
             folder_id: The id of the folder to upload the file to.
             file_name: The name of the file to upload.
             subfolder_name: The name of the subfolder to upload the file to.
+            root_folder: The name of the root folder to upload the file to.
         '''
         if(file_path == None or file_path == ''):
             self.windows.write("\u2612Aucun fichier sélectionné")
@@ -114,22 +163,18 @@ class FileOrganiser:
             self.windows.change_button_color("red")
             return
         try:
-            # Vérifier si le sous-dossier existe déjà
-            query = "mimeType='application/vnd.google-apps.folder' and trashed = false and name='" + subfolder_name + "' and parents in '" + folder_id + "'"
+            # Vérifier si le sous-dossier existe
+            subfolder_id = None
+            query = "mimeType='application/vnd.google-apps.folder' and trashed = false and name='" + subfolder_name + "' and '" + folder_id + "' in parents"
             results = self.service.files().list(q=query,fields="nextPageToken, files(id, name)").execute()
             files = results.get("files", [])
-
-            # Si le sous-dossier n'existe pas, le créer
             if not files:
-                folder_metadata = {
-                    'name': subfolder_name,
-                    'mimeType': 'application/vnd.google-apps.folder',
-                    'parents': [folder_id]
-                }
-                folder = self.service.files().create(body=folder_metadata, fields='id').execute()
-                subfolder_id = folder.get('id')
+                # Créer le sous-dossier
+                file_metadata = {'name': subfolder_name, 'mimeType': 'application/vnd.google-apps.folder', 'parents': [folder_id]}
+                file = self.service.files().create(body=file_metadata, fields='id').execute()
+                subfolder_id = file.get('id')
             else:
-                subfolder_id = files[0]['id']
+                subfolder_id = files[0].get('id')
 
             # Téléverser le fichier dans le sous-dossier
             file_metadata = {'name': file_name, 'parents': [subfolder_id]}
@@ -138,20 +183,25 @@ class FileOrganiser:
             self.windows.write("\u2611Fichier téléversé avec succès")
             self.windows.change_button_color("green")
         except HttpError as error:
-            self.windows.write(F'\u2612Erreur lors du téléversement du fichier: {error}')
+            self.windows.write('\u2612Erreur lors du téléversement du fichier: '+ error)
             self.windows.change_button_color("red")
-    def class_files(self, name):
+    def class_files(self, name, folders):
         """Classify the files into their respective folders.
 
         Args:
             files: The list of files to classify.
         """
+
+        if(name == None or name == ''):
+            self.windows.write("\u2612Aucun nom de fichier sélectionné")
+            self.windows.change_button_color("red")
+            return
         
-        selected_folders = self.folders.copy()
+        selected_folders = folders.copy()
         returned_folder = selected_folders.copy()
         char_index = 0;
 
-        while(len(returned_folder) > 1):
+        while(len(returned_folder) > 1 or char_index < len(name)):
             char_integer = char_to_int(name[char_index])
             for i in range(len(selected_folders)):
                 if(len(selected_folders[i].from_index) <= char_index or len(selected_folders[i].to_index) <= char_index):
@@ -164,6 +214,11 @@ class FileOrganiser:
             char_index += 1
         if len(returned_folder) == 1:
             return returned_folder[0]
+        if len(returned_folder) == 0:
+            self.windows.write("\u2612Aucun dossier adapté trouvé")
+            self.windows.change_button_color("red")
+            return None
+        
         max_index = 0
         max = len(returned_folder[0].from_index)
         for folder in returned_folder:
@@ -171,7 +226,7 @@ class FileOrganiser:
                 max = len(folder.from_index)
                 max_index = returned_folder.index(folder)
         return returned_folder[max_index]
-    def send_file_to(self, name, activity):
+    def send_file_to(self, name, activity, root_folder):
         """Upload a new file to a folder.
 
         Args:
@@ -179,16 +234,27 @@ class FileOrganiser:
             activity (str): The activity of the file to upload.
         """
         # Create the file metadata
-        folder = self.class_files(name)
+        if(name == None or name == ''):
+            self.windows.write("\u2612Aucun nom de fichier sélectionné")
+            self.windows.change_button_color("red")
+            return
+        if(root_folder == None):
+            self.windows.write("\u2612Aucun dossier racine valable sélectionné")
+            self.windows.change_button_color("red")
+            return
+        folder = self.class_files(name, root_folder["subfolders"])
+        if(folder == None):
+            return
+       
         #create custom name Month-Year - name - activity
         date = datetime.datetime.now()
-        name_to_sort = f"{date.month}-{date.year} - {activity} - {name}"
+        name_to_sort = str(date.month)+"-"+str(date.year)+"-"+str(activity)+"-"+name
         self.upload_file(self.file_to_sort, folder.id, name_to_sort, name)
 class Folder:
     """A class to represent a folder.
     """
 
-    def __init__(self, rawfile: dict, id: str):
+    def __init__(self, rawfile, id):
         self.id = id
         self.name = rawfile["name"]
         frome = rawfile["from"]
@@ -218,10 +284,11 @@ def browse_file(organiser):
     file_path = filedialog.askopenfilename()
     organiser.set_file_to_sort(file_path)
     organiser.windows.write("\u2611Fichier sélectionné")
-def send_file(organiser, name_to_sort, category):
+    organiser.windows.write_pdf_info(retrieve_pdf_info(file_path))
+def send_file(organiser, name_to_sort, category, root_folder):
     organiser.windows.write("\u2610Fichier en cours d'envoi")
     organiser.windows.redraw()
-    organiser.send_file_to(name_to_sort, category)
+    organiser.send_file_to(name_to_sort, category, root_folder)
      
 
 
@@ -255,29 +322,33 @@ def main():
     raw = json.load(open('settings.json', 'r',encoding="utf8"))
     foldersRef = raw["folders"]
     categories = raw["categories"]
-    folders=[]
+    rootFolders = raw["root-folders"]
     try:
         # Create the service object for interacting with the API.
         service = build('drive', 'v3', credentials=creds)
 
         # Set up the query parameters
-        query = "mimeType='application/vnd.google-apps.folder'"
+        query = "mimeType='application/vnd.google-apps.folder'"     
+        organizer = FileOrganiser(service)
 
-        # Retrieve the list of folders
-        results = service.files().list(q=query, fields='nextPageToken, '
-                                                'files(id, name)').execute()
-        items = results.get('files', [])
-
-        # Identify folders
-        for item in items:
-            for file in foldersRef:
-                if item['name'] == file['name']:
-                    folders.append(Folder(file, item['id']))
-                    
-        organizer = FileOrganiser(folders, service)
 
     except HttpError as error:
         print('An error occurred'+error)
+        return
+
+    #Retrieve all root folders
+    root_folders = {}
+    for root_folder in rootFolders:
+        temp = retrieve_folder_from_name(service, root_folder)
+        if(temp == None):
+            continue
+        root_folders[root_folder] = temp
+        root_folders[root_folder]["subfolders"] = []
+        allFolders = retrieve_folders(service, root_folder)
+        for file in foldersRef:
+            for item in allFolders:
+                if item['name'] == file['name']:
+                    root_folders[root_folder]["subfolders"].append(Folder(file, item['id']))
 
 
     # Création de la fenêtre principale de taille 640x480
@@ -285,12 +356,19 @@ def main():
     window.geometry("640x480")
     window.title("Cab Sort")
 
+    # Création 
+
     # Création du champ de saisie pour le nom et le prénom
     name_entry = tk.Entry(window)
-    name_entry.pack()
     selected = tk.StringVar()
     selected.set(categories[0])  # On définit l'option sélectionnée par défaut
     
+    root_selected = tk.StringVar()
+    root_selected.set(rootFolders[0])  # On définit l'option sélectionnée par défaut
+
+    #Info pdf box
+    info_pdf_box = tk.Label(window, text="Aucun fichier sélectionné")
+
     #Creation d'un text dialog avec texte modifiable
     dialog_box = tk.Label(window, text="Historique\nAucun fichier sélectionné")
 
@@ -300,17 +378,24 @@ def main():
     
     # Création des boutons
     dropdown = tk.OptionMenu(window, selected, *categories)
+    root_selection = tk.OptionMenu(window, root_selected, *rootFolders)
     browse_button = tk.Button(text="Parcourir", command= lambda : browse_file(organizer))
-    send_button = tk.Button(text="Envoyer", command= lambda  : send_file(organizer, name_entry.get(), selected.get()))
+    send_button = tk.Button(text="Envoyer", command= lambda  : send_file(organizer, name_entry.get(), selected.get(), root_folders[root_selected.get()]))
 
-    # Packing
+    # Packing all items on the left side vertically
+    name_entry.pack()
     dropdown.pack()
+    root_selection.pack()
     browse_button.pack()
     send_button.pack()
     dialog_box.pack()
 
+    #Packing the pdf info on the right side
+    #info_pdf_box.pack(side="right")
+
+
     # Set windows
-    organizer.set_windows(Windows(window,send_button,dialog_box))
+    organizer.set_windows(Windows(window,send_button,dialog_box, info_pdf_box))
 
     # Affichage de la fenêtre
     window.mainloop()
